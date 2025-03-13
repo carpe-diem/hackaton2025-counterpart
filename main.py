baa536011641cfde385b2f51a3d98a10837daa81
Alberto Paparelli <alberto@paparelli.com.ar>
2025-03-13 09:00:35 -0300

    fix req

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from openai import OpenAI
 import os
 from pdf import text
+from dotenv import load_dotenv
 
+load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))  # Replace with your API key
 
